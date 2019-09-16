a03fd139a687ee02e510a74209784fbafbde1367
ite19-3c-2@M303.UC-BCF.EDU.PH <ite19-3c-2@M303.UC-BCF.EDU.PH>
2019-09-16 23:46:43 +0300

    eloq

---
+++ database/migrations/2019_09_06_175449_create_project_teams_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_teams', function (Blueprint $table) {
+            $table->bigIncrements('team_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_teams');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', 'ProjectsController@index');
+Route::get('/projects/add', 'ProjectsController@create');
+Route::post('projects/store', 'ProjectsController@store');
