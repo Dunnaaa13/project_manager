dc767ebd22df583158d86f6e69c41dd425b6f37f
dunnaaa13 <donnarosebulatao@gmail.com>
2019-10-12 21:57:30 +0800

    haha

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,17 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Project Manager</title>
-
-    <style>
-            html, body {
-                background-color: #cfc9c4;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-        </style>
+    
   </head>
   <body>
     @yield('content')
